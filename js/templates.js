311953cdd95d2e0e2087a9cbe0e92d38c64e93be
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2023-11-29 19:30:01 +0100

    Update templates.js

--- js/templates.js
+++ js/templates.js
@@ -25,3 +25,37 @@ function exampleHTML() {
     </div>`;
     return htmlText;
 }
+
+function navbarHTML() {
+    htmlText = "";
+    htmlText = /*html*/`
+    <div class="sidebar">
+        <div>
+            <img class="logo" src="./img/logo-white.svg" alt="">
+        </div>
+        <div class="menu-section">
+            <ul>
+                <li>
+                    <img class="icon" src="./img/summary_sidebar.svg" alt="Summary Icon">
+                    <a href="summary.html">Summary</a>
+                </li>
+                <li>
+                    <img class="icon" src="./img/add_task_sidebar.svg" alt="Add Task Icon">
+                    <a href="about.html">Ad Task</a>
+                </li>
+                <li>
+                    <img class="icon" src="./img/Board_sidebar.svg" alt="Board Icon">
+                    <a href="board.html">Board</a>
+                </li>
+                <li>
+                    <img class="icon" src="./img/Contacts_sidebar.svg" alt="Contacts Icon">
+                    <a href="join.html">Contacts</a>
+                </li>
+            </ul>
+        </div>
+        <li class="footer-section">
+            <a class="footertext" href="join.html">Privacy Policy</a>
+            <a class="footertext" href="desktop.html">legal Notice</a>
+        </li>
+    </div>`
+}
