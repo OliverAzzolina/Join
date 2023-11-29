1db292e4c6509696c249fe1f3ef855a698dd8b1d
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2023-11-29 19:29:57 +0100

    Update add-task.js

--- js/add-task.js
+++ js/add-task.js
@@ -1,3 +1,34 @@
-function init() {
+document.addEventListener('DOMContentLoaded', function () {
+    btnEventListener();
+});
+
+    var selectedPriority = '';
+
+    function btnEventListener() {
+        document.querySelectorAll('.add-task-form-btn').forEach(function (button) {
+            button.addEventListener('click', function () {
+                selectedPriority = this.getAttribute('task-priority');
+                highlightSelectedButton(this); //TODO: highlightSelectedButton()
+            });
+        });
+    }
+
+    function highlightSelectedButton(selectedButton) {
+        // TODO: highlight the btn
+    }
+
+    function addTask() {
+        event.preventDefault(); // Prevent the page from reloading
     
-}
+        var taskData = {
+            title: document.getElementById('add-task-title-input').value,
+            description: document.getElementById('add-task-description-input').value,
+            priority: selectedPriority,
+            dueDate: document.getElementById('add-task-date-input').value,
+            category: document.querySelectorAll('.add-task-form-dropdown')[0].value,
+            assignedTo: document.querySelectorAll('.add-task-form-dropdown')[1].value,
+            subtasks: document.querySelector('.add-task-form-input[type="text"]').value
+        };
+    
+        console.log(taskData);
+    }
