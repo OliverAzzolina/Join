0fdab3d2d61c97de3f9d070625c9cad751ced55f
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2023-12-03 15:45:18 +0100

    moved tasks array to arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -1,1 +1,25 @@
 let contacts = [];
+let tasks = [
+    {
+      title: "Kochwelt Page & Recipe Recommender",
+      description: "Build start page with recipe recommendation...",
+      assignedto: [],
+      duedate: "10/05/2023",
+      prio: "Medium",
+      category: "User Story",
+      subtasks: ['Implement Recipe Recommendation','Start Page Layout'],
+      subTasksDone: ['Implement Recipe Recommendation'],
+      status: "open",
+    },
+    {
+      title: "Title",
+      description: "Description",
+      assignedto: ['AZ', 'RF', 'XY'],
+      duedate: "10/05/2023",
+      prio: "Medium",
+      category: "Technical Task",
+      subtasks: ['Task1','Task2', 'Task3'],
+      subTasksDone: ['Task1', 'Task2'],
+      status: "in-progress",
+    }
+  ];
