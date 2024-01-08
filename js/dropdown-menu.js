6519bb705ddfb4af0934888251ddb626f50b36f2
Pixelschmied <RainerZaddach@googlemail.com>
2024-01-08 22:47:13 +0100

    removed

--- js/dropdown-menu.js
+++
@@ -1,6 +0,0 @@
-// Old dropdown menu script -> moved to utils.js
-
-function toggleDropdown() {
-    var dropdown = document.getElementById("dropdownMenu");
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-}
