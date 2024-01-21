557c01377a06436c6bd8d5ae436611543e249438
Pixelschmied <RainerZaddach@googlemail.com>
2024-01-21 21:40:45 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,19 @@ function toggleDropdown() {
     var dropdown = document.getElementById("dropdownMenu");
     dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 }
+
+
+function generateRandomId() {
+    return Math.floor(100000 + Math.random() * 900000);
+}
+
+
+function clearDataBase() {
+    localStorage.clear();
+    location.reload();
+}
+
+
+function generateRandomColor(){
+    return randomColor = Math.floor(Math.random()*16777215).toString(16);
+  }
