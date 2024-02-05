64d52830da9ae201497186364797cd4cb0ff3102
Pixelschmied <RainerZaddach@googlemail.com>
2024-02-05 19:48:47 +0100

    added logout function

--- js/utils.js
+++ js/utils.js
@@ -19,3 +19,8 @@ function generateRandomColor(){
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     return "#" + randomColor
   }
+
+  function logout() {
+    localStorage.clear();
+    window.location.href = 'index.html';
+  }
