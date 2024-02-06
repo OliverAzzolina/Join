65b3c8721e119c47bd1b5da2073db196b80789bf
Marcel Arnold <caramello-code@gmx.ch>
2024-02-06 20:34:09 +0100

    updated Files

---
+++ js/dropdown-menu.js
@@ -0,0 +1,6 @@
+// Old dropdown menu script -> moved to utils.js
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdownMenu");
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
