4d5e891d4e88453d36ba581b4d2cb41ca43521dc
Pixelschmied <RainerZaddach@googlemail.com>
2024-02-13 22:41:50 +0100

    cleanup

--- js/storage.js
+++ js/storage.js
@@ -32,15 +32,3 @@ async function getRemoteArray(key) {
     const response = JSON.parse(responseJson);
     console.log(response)
 }
-
-
-//async function setItem(key, value) {
-//    const payload = { key, value, token: STORAGE_TOKEN };
-//    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
-//    .then(res => res.json());
-//}
-//
-//async function getItem(key) {
-//    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-//    return fetch(url).then(res => res.json());
-//}
