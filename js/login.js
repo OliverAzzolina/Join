c4da4e0122deb019d146b955946e49b6ddbb1225
Pixelschmied <RainerZaddach@googlemail.com>
2024-02-13 22:42:26 +0100

    moved some functions
    moved to index.js

--- js/login.js
+++ js/login.js
@@ -1,30 +1,3 @@
-function loadLoginListeners() {
-    document.getElementById('login-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        loginUser();
-    });
-}
-
-async function loginUser() {
-    let email = document.getElementById('email-input').value.trim();
-    let password = document.getElementById('password-input').value.trim();
-    let user = await findUser(email, password);
-    if (user) {
-        console.log('User found:', user.email);
-        localStorage.setItem("userId", user.userId);
-        window.location.href = 'summary.html';
-    } else {
-        console.error('User not found.');
-    }
-}
-
-async function findUser(email, password) {
-        let usersJson = await getItem('users');
-        let users = JSON.parse(usersJson);
-        return users.find(user => user.email === email && user.password === password);
-        
-}
-
 async function guestLogin() {
     let usersJson = await getItem('users');
     try {
@@ -76,3 +49,5 @@ async function showUsersOnRemoteStorage() {
         console.error("Fehler beim Abrufen oder Parsen der Nutzerdaten:", error);
     }
 }
+
+
